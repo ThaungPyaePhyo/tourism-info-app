2aa643e848c798430b16822c09031e59ffbb6331
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-08-15 23:27:27 +0700

    fix bugs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') ?? 'Tourism-Info-App' }}</title>
     @vite('resources/css/app.css')
 </head>
 <body>
