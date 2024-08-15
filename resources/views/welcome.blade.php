d98bbfba81c092d50abe957c42937a8959a3a01b
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-08-15 23:34:59 +0700

    add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') ?? 'Tourism-Info-App' }}</title>
     @vite('resources/css/app.css')
+    <link rel="icon" type="image/png" href="{{ asset('tourism.jpeg') }}">
 </head>
 <body>
 <div id="app">
